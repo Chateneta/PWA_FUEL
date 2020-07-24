88f0c8c783e45db0af9a7d79b643a206b4a521bb
Antoine Chatenet <47383406+Chateneta@users.noreply.github.com>
2020-07-24 12:00:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include('php\fct.php');
+// getData();
+?>
+<head>
+  <title>PWA_FUEL</title>
+  <link rel="stylesheet" href="css\style.css">
+  <link rel="stylesheet" href="css\bootstrap.min.css">
+  <script src="js\jquery.js"></script>
+  <script src="js\main.js"></script>
+</head>
+<body>
+  <button class="bg-warning" id="go" onclick="launch()">GO</button>
+  <div class="container">
+    <div id="test"></div>
+  </div>
+</body>
